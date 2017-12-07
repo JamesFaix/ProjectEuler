b986cbd138fc706180422e362467059ead863caf
James Faix <jamesthefaix@gmail.com>
2017-12-07 09:00:43 -0500

    Problem3

---
+++ ProjectEuler/Problem3.fs
@@ -0,0 +1,40 @@
+﻿(* Problem 3: Largest Prime Factor
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+*)
+
+module Problem3
+
+open System
+
+let getAnswer =
+    let product = 600851475143L
+    
+    let factorLimit = Convert.ToInt64(Math.Floor(Math.Sqrt(Convert.ToDouble(product))))
+    
+    let mutable primes = [2L]
+    
+    let isPrime (n : Int64) = 
+        primes 
+        |> List.exists(fun p -> n % p = 0L) 
+        |> not
+
+    let isFactor p =
+        if product % p = 0L 
+        then Some p 
+        else None
+
+    for n in [3L..factorLimit] do
+        if isPrime n 
+        then do primes <- n::primes
+        else ()
+
+    primes 
+    |> Seq.pick isFactor
+
+(*
+    You are the 410449th person to have solved this problem.
+*)
--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -1,6 +1,7 @@
 ﻿[<EntryPoint>]
 let main argv = 
-//    printfn "The answer to Problem 1 is %A" Problem1.getAnswer
-    printfn "The answer to Problem 2 is %A" Problem2.getAnswer
+    //printfn "The answer to Problem 1 is %A" Problem1.getAnswer
+    //printfn "The answer to Problem 2 is %A" Problem2.getAnswer
+    printfn "The answer to Problem 3 is %A" Problem3.getAnswer
     System.Console.ReadLine() |> ignore
     0
