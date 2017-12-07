79f9aad1514bd25b3b2b801505dd84a02864f917
James Faix <jamesthefaix@gmail.com>
2017-12-06 20:40:37 -0500

    Problem2

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -1,5 +1,6 @@
 ﻿[<EntryPoint>]
 let main argv = 
-    printfn "The answer to Problem 1 is %A" Problem1.getAnswer
+//    printfn "The answer to Problem 1 is %A" Problem1.getAnswer
+    printfn "The answer to Problem 2 is %A" Problem2.getAnswer
     System.Console.ReadLine() |> ignore
     0
