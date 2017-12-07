fdac4723e29b55aa765b3622561dcd61d5d0c382
James Faix <jamesthefaix@gmail.com>
2017-12-07 10:22:00 -0500

    Problem6

---
+++ ProjectEuler/Problem6.fs
@@ -0,0 +1,40 @@
+﻿(* Problem 6: Sum square difference
+
+The sum of the squares of the first ten natural numbers is,
+
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first 
+ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first 
+one hundred natural numbers and the square of the sum.
+
+*)
+
+module Problem6
+
+let getAnswer =
+    let square n = n * n
+
+    let sumOfSquares n =
+        Seq.initInfinite(fun i -> square (i + 1))
+        |> Seq.take n
+        |> Seq.sum
+
+    let squareOfSum n =        
+        Seq.initInfinite(fun i -> i + 1)
+        |> Seq.take n
+        |> Seq.sum
+        |> square
+
+    let n = 100
+    (squareOfSum n) - (sumOfSquares n)
+
+(*
+    You are the 377290th person to have solved this problem.
+*)
--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -4,6 +4,7 @@ let main argv =
     //printfn "The answer to Problem 2 is %A" Problem2.getAnswer
     //printfn "The answer to Problem 3 is %A" Problem3.getAnswer
     //printfn "The answer to Problem 4 is %A" Problem4.getAnswer
-    printfn "The answer to Problem 5 is %A" Problem5.getAnswer
+    //printfn "The answer to Problem 5 is %A" Problem5.getAnswer
+    printfn "The answer to Problem 6 is %A" Problem6.getAnswer
     System.Console.ReadLine() |> ignore
     0
