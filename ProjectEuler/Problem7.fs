e5325c9647e604d6b47f746e1d2c4de26ab89dce
James Faix <jamesthefaix@gmail.com>
2017-12-07 10:32:14 -0500

    Problem7

---
+++ ProjectEuler/Problem7.fs
@@ -0,0 +1,33 @@
+﻿(* Problem 7: 10001st prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+*)
+
+module Problem7
+
+open System
+
+let getAnswer =
+    let mutable primes = [2L]
+    
+    let isPrime (n : Int64) = 
+        primes 
+        |> List.exists(fun p -> n % p = 0L) 
+        |> not
+        
+    let mutable n = 3L
+
+    while (primes |> List.length) < 10001 do
+        if isPrime n
+        then do primes <- n::primes
+        else ()
+        n <- n+1L
+
+    primes.Head
+
+(*
+    You are the 323122nd person to have solved this problem.
+*)
--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -5,6 +5,7 @@ let main argv =
     //printfn "The answer to Problem 3 is %A" Problem3.getAnswer
     //printfn "The answer to Problem 4 is %A" Problem4.getAnswer
     //printfn "The answer to Problem 5 is %A" Problem5.getAnswer
-    printfn "The answer to Problem 6 is %A" Problem6.getAnswer
+    //printfn "The answer to Problem 6 is %A" Problem6.getAnswer
+    printfn "The answer to Problem 7 is %A" Problem7.getAnswer
     System.Console.ReadLine() |> ignore
     0
