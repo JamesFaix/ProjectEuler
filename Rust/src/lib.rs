1aa0239ed1e30ea695245a074c4c5485409a4c10
James Faix <jamesthefaix@gmail.com>
2018-01-28 11:50:13 -0500

    Created rust project

---
+++ Rust/src/lib.rs
@@ -0,0 +1,11 @@
+mod problem16;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn problem16_test() {
+        let result = super::problem16::get_answer();
+        assert_eq!(result, 0);
+    }
+}
