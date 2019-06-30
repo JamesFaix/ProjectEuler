26566bf14b70782f37a16357fdb649eca1d220e1
James Faix <jamesthefaix@gmail.com>
2019-06-30 14:50:10 -0400

    Solved problem 20

--- Java/src/main/java/projectEuler/Problem20.java
+++ Java/src/main/java/projectEuler/Problem20.java
@@ -9,8 +9,53 @@ package projectEuler;
     Find the sum of the digits in the number 100!
 */
 
+import java.util.*;
+import java.math.BigInteger;
+
 public class Problem20 {
+
+    //Normally you would memoize w/ a HashMap, but in this case the keys will always be
+    //integers starting at 0, so a list works fine.
+    //A hashmap may provide faster arbitrary lookup though.
+    private static final ArrayList<BigInteger> cache = new ArrayList<>();
+
+    static {
+        cache.add(BigInteger.ONE); //0! = 1
+    }
+
     public static int getAnswer() {
-        throw new UnsupportedOperationException();
+        return sumDigits(factorial(100));
+    }
+
+    public static BigInteger factorial(int n) {
+        var size = cache.size();
+
+        if (size > n) {
+            return cache.get(n);
+        }
+
+        var lastIndex = size-1;
+        var lastValue = cache.get(lastIndex);
+
+        for (var i = (lastIndex+1); i <= n; i++) {
+            var currentValue = lastValue.multiply(BigInteger.valueOf(i));
+            cache.add(currentValue);
+            lastValue = currentValue;
+        }
+
+        return lastValue;
+    }
+
+    public static int sumDigits(BigInteger n) {
+        var str = n.toString(/*radix*/ 10);
+        var sum = 0;
+        for (char c : str.toCharArray()) {
+            sum += Character.getNumericValue(c);
+        }
+        return sum;
     }
 }
+
+/*
+    You are the 179680th person to have solved this problem.
+*/
--- Java/src/test/java/projectEuler/Problem20Test.java
+++ Java/src/test/java/projectEuler/Problem20Test.java
@@ -3,11 +3,44 @@ package projectEuler;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 public class Problem20Test {
 
     @Test public void checkAnswer() {
-        var expected = 0;
+        var expected = 648;
         var actual = Problem20.getAnswer();
         assertEquals("Problem 20 returns the correct answer", expected, actual);
+    }
+
+    @Test public void factorial_shouldWorkFor0() {
+        var expected = BigInteger.ONE;
+        var actual = Problem20.factorial(0);
+        assertEquals(expected, actual);
+    }
+
+    @Test public void factorial_shouldWorkFor2() {
+        var expected = BigInteger.valueOf(2);
+        var actual = Problem20.factorial(2);
+        assertEquals(expected, actual);
+    }
+
+    @Test public void factorial_shouldWorkFor4() {
+        var expected = BigInteger.valueOf(24);
+        var actual = Problem20.factorial(4);
+        assertEquals(expected, actual);
+    }
+
+    @Test public void factorial_shouldWorkFor100() {
+        //Looked this up w/ WolframAlpha
+        var expected = new BigInteger(
+            "933262154439441526816992388562667" +
+            "004907159682643816214685929638952" +
+            "175999932299156089414639761565182" +
+            "862536979208272237582511852109168" +
+            "64000000000000000000000000");
+
+        var actual = Problem20.factorial(100);
+        assertEquals(expected, actual);
     }
 }
