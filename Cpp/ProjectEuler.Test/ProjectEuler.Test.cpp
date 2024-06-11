342f64ed2a117dfda0afba063d8518118756d30f
James Faix <jamesthefaix@gmail.com>
2024-06-11 10:33:31 -0700

    Replaced console app project with library and test projects

---
+++ Cpp/ProjectEuler.Test/ProjectEuler.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProjectEulerTest
+{
+	TEST_CLASS(ProjectEulerTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
