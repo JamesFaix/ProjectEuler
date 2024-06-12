56a0f5ef464088e47680c46b58ae6d7b936ab897
James Faix <jamesthefaix@gmail.com>
2024-06-11 19:54:12 -0700

    Add problem 25 instructions comment

--- Cpp/ProjectEuler.StaticLib/ProjectEuler.StaticLib.cpp
+++ Cpp/ProjectEuler.StaticLib/ProjectEuler.StaticLib.cpp
@@ -4,7 +4,31 @@
 #include "pch.h"
 #include "framework.h"
 
+/* 
+	Problem 25:
+
+	The first 12 terms of the Fibonacci sequence are:
+		F1  =   1
+		F2  =   1
+		F3  =   2
+		F4  =   3
+		F5  =   5
+		F6  =   8
+		F7  =  13
+		F8  =  21
+		F9  =  34
+		F10 =  55
+		F11 =  89
+		F12 = 144
+
+	The 12th term, F12, is the first to contain three digits.
+
+	What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+*/
+
 int solveProblem25()
 {
+
+
 	return 3;
 }
