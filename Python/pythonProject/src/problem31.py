65789f1ef62238e4752073e26a2077f453df2fa6
James Faix <jamesthefaix@gmail.com>
2024-06-24 19:24:07 -0700

    Add instructions and failing tests

--- Python/pythonProject/src/problem31.py
+++ Python/pythonProject/src/problem31.py
@@ -1,4 +1,33 @@
 import math
+from typing import NewType
+
+# In the United Kingdom the currency is made up of pound (£) and pence (p).
+# There are eight coins in general circulation:
+#     1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+#
+# It is possible to make £2 in the following way:
+#     1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+#
+# How many different ways can £2 be made using any number of coins?
+
+
+Coin = NewType('Coin', int)
+
+
+valid_coins = [
+    Coin(1),
+    Coin(2),
+    Coin(5),
+    Coin(10),
+    Coin(20),
+    Coin(50),
+    Coin(100),
+    Coin(200)
+]
+
+
+def find_combinations_for_sum(sum: int):
+    return []
 
 
 def solve() -> int:
--- Python/pythonProject/tests/test_problem31.py
+++ Python/pythonProject/tests/test_problem31.py
@@ -1,11 +1,20 @@
 import unittest
-from src import problem31
+from src.problem31 import find_combinations_for_sum, solve, Coin
 
 
 class Problem31Tests(unittest.TestCase):
 
+    def test_find_combinations_for_sum_5(self):
+        expected = [
+            [Coin(1), Coin(1), Coin(1), Coin(1), Coin(1)],
+            [Coin(1), Coin(2), Coin(2)],
+            [Coin(5)]
+        ]
+        actual = find_combinations_for_sum(5)
+        self.assertEqual(actual, expected)
+
     def test_solve(self):
-        actual = problem31.solve()
+        actual = solve()
         expected = -1
 
         self.assertEqual(actual, expected)
