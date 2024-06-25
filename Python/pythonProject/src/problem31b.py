306ca37325f2076f2a4db6fba74cffa9ad1f79c3
James Faix <jamesthefaix@gmail.com>
2024-06-24 22:41:26 -0700

    Solved 31

--- Python/pythonProject/src/problem31b.py
+++ Python/pythonProject/src/problem31b.py
@@ -1,5 +1,4 @@
 from typing import NewType
-from functools import reduce
 from math import floor
 
 # In the United Kingdom the currency is made up of pound (£) and pence (p).
@@ -10,14 +9,6 @@ from math import floor
 #     1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 #
 # How many different ways can £2 be made using any number of coins?
-
-
-Coin = NewType('Coin', int)
-valid_coin_values = [1, 2, 5, 10, 20, 50, 100, 200]
-
-
-def max_of_each_within_sum(total: int):
-    return map(lambda x: (x, floor(total / x)), valid_coin_values)
 
 
 def integer_divide(dividend: int, divisor: int):
@@ -40,26 +31,39 @@ def get_combination_value(c: Combination) -> int:
 
 
 def get_all_possible_combinations(total: int):
-    def range_of_coin(value: int):
-        return range(0, integer_divide(total, value) + 1)
+    def range_of_coin_optimized(value: int, subtotal: int):
+        return range(0, integer_divide(subtotal, value) + 1)
 
     def is_correct_value(c: Combination):
         return get_combination_value(c) == total
 
     possible_combinations: list[Combination] = []
 
-    for p1 in range_of_coin(1):
-        for p2 in range_of_coin(2):
-            for p5 in range_of_coin(5):
-                for p10 in range_of_coin(10):
-                    for p20 in range_of_coin(20):
-                        for p50 in range_of_coin(50):
-                            for p100 in range_of_coin(100):
-                                for p200 in range_of_coin(200):
+    for p200 in range_of_coin_optimized(200, total):
+        p100_remaining = total - (200 * p200)
+        for p100 in range_of_coin_optimized(100, p100_remaining):
+            p50_remaining = p100_remaining - (100 * p100)
+            for p50 in range_of_coin_optimized(50, p50_remaining):
+                p20_remaining = p50_remaining - (50 * p50)
+                for p20 in range_of_coin_optimized(20, p20_remaining):
+                    p10_remaining = p20_remaining - (20 * p20)
+                    for p10 in range_of_coin_optimized(10, p10_remaining):
+                        p5_remaining = p10_remaining - (10 * p10)
+                        for p5 in range_of_coin_optimized(5, p5_remaining):
+                            p2_remaining = p5_remaining - (5 * p5)
+                            for p2 in range_of_coin_optimized(2, p2_remaining):
+                                p1_remaining = p2_remaining - (2 * p2)
+                                for p1 in range_of_coin_optimized(1, p1_remaining):
                                     c: Combination = (p1, p2, p5, p10, p20, p50, p100, p200)
-                                    possible_combinations.append(c)
+                                    if is_correct_value(c):
+                                        possible_combinations.append(c)
 
-    possible_combinations = list(filter(is_correct_value, possible_combinations))
     possible_combinations.sort()
 
     return possible_combinations
+
+
+def solve() -> int:
+    return len(get_all_possible_combinations(200))
+
+# You are the 91062nd person to have solved this problem.
--- Python/pythonProject/tests/test_problem31b.py
+++ Python/pythonProject/tests/test_problem31b.py
@@ -4,20 +4,6 @@ from src.problem31b import *
 
 class Problem31Tests(unittest.TestCase):
 
-    def test_max_of_each_within_sum(self):
-        actual = list(max_of_each_within_sum(200))
-        expected = [
-            (1, 200),
-            (2, 100),
-            (5, 40),
-            (10, 20),
-            (20, 10),
-            (50, 4),
-            (100, 2),
-            (200, 1)
-        ]
-        self.assertEqual(actual, expected)
-
     def test_get_all_possible_combinations_5(self):
         actual = get_all_possible_combinations(5)
 
@@ -28,5 +14,11 @@ class Problem31Tests(unittest.TestCase):
             (0, 0, 1, 0, 0, 0, 0, 0)
         ]
         expected.sort()
+
+        self.assertEqual(actual, expected)
+
+    def test_solve(self):
+        actual = solve()
+        expected = 73682
 
         self.assertEqual(actual, expected)
