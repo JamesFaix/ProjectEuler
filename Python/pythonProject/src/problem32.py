70623fdc815343582855c22c46f69bf29ff994d9
James Faix <jamesthefaix@gmail.com>
2024-06-24 23:42:13 -0700

    Solved 32

--- Python/pythonProject/src/problem32.py
+++ Python/pythonProject/src/problem32.py
@@ -17,25 +17,53 @@ def digits_overlap(digits1, digits2):
     return any(check in digits1 for check in digits2)
 
 
-def solve() -> int:
+def get_possible_factors():
+    def is_valid(n):
+        digits = get_digits(n)
+        return 0 not in digits and are_digits_distinct(digits)
+
+    xs = range(0, factor_upper_bound + 1)
+    xs = filter(is_valid, xs)
+    return list(xs)
+
+
+def get_possible_factor_pairs():
+    xs = []
+
+    factors = get_possible_factors()
+
+    for a in factors:
+        digits_a = get_digits(a)
+        for b in factors:
+            digits_b = get_digits(b)
+
+            if not digits_overlap(digits_a, digits_b):
+                xs.append((a, b))
+
+    return xs
+
+
+def get_pandigital_products():
     results = []
 
-    for a in range(0, factor_upper_bound + 1):
-        digits_a = get_digits(a)
-        if are_digits_distinct(digits_a):
+    for (a, b) in get_possible_factor_pairs():
+        x = a * b
+        digits = get_digits(a) + get_digits(b) + get_digits(x)
 
-            for b in range(0, factor_upper_bound + 1):
-                digits_b = get_digits(b)
-                if (are_digits_distinct(digits_b)
-                    and not digits_overlap(digits_a, digits_b)):
+        if (len(digits) == 9
+                and 0 not in digits
+                and are_digits_distinct(digits)
+                and (b, a, x) not in results):
 
-                    x = a * b
-                    digits_x = get_digits(x)
+            results.append((a, b, x))
 
-                    if (are_digits_distinct(digits_x)
-                        and not digits_overlap(digits_a, digits_x)
-                        and not digits_overlap(digits_b, digits_x)):
+    return results
 
-                        results.append((a, b, x))
 
-    return len(results)
+def solve() -> int:
+    results = get_pandigital_products()
+    products = map(lambda tup: tup[2], results)
+    products = list(set(products))
+    return sum(products)
+
+# You are the 76389th person to have solved this problem.
--- Python/pythonProject/tests/test_problem32.py
+++ Python/pythonProject/tests/test_problem32.py
@@ -4,6 +4,22 @@ from src.problem32 import *
 
 class Problem32Tests(unittest.TestCase):
 
+    def test_get_pandigital_products(self):
+        actual = get_pandigital_products()
+        expected = [
+            (4, 1738, 6952),
+            (4, 1963, 7852),
+            (12, 483, 5796),
+            (18, 297, 5346),
+            (27, 198, 5346),
+            (28, 157, 4396),
+            (39, 186, 7254),
+            (42, 138, 5796),
+            (48, 159, 7632)
+        ]
+
+        self.assertEqual(actual, expected)
+
     def test_solve(self):
         actual = solve()
         expected = -1
