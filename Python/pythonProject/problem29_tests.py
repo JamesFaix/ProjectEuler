83a585791d77b45389f5bdb8e0c9ef367d9e045b
James Faix <jamesthefaix@gmail.com>
2024-06-24 17:37:19 -0700

    Solve 29

---
+++ Python/pythonProject/problem29_tests.py
@@ -0,0 +1,15 @@
+import unittest
+import problem29
+
+
+class Problem29Tests(unittest.TestCase):
+
+    def test_solve(self):
+        actual = problem29.solve()
+        expected = 0
+
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
