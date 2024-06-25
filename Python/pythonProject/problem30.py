c9d520bb971d8f0bca40ad1bad48e0b802b349bf
James Faix <jamesthefaix@gmail.com>
2024-06-24 18:43:47 -0700

    Solved problem 30

--- Python/pythonProject/problem30.py
+++ Python/pythonProject/problem30.py
@@ -10,8 +10,18 @@ def is_sum_of_digits_to_power(number: int, power: int) -> bool:
         return math.pow(n, power)
 
     digits = get_digits(number)
-    return number == sum(list(map(to_pow, digits)))
+    if len(digits) < 2:
+        return False
+    else:
+        return number == sum(list(map(to_pow, digits)))
 
 
-def solve() -> int:
-    return 0
+def get_numbers_that_are_sum_of_powers_of_their_digits(power: int, limit: int) -> list:
+    rng = range(1, limit + 1)
+    return list(filter(lambda x: is_sum_of_digits_to_power(x, power), rng))
+
+
+def solve(limit: int) -> int:
+    return sum(get_numbers_that_are_sum_of_powers_of_their_digits(5, limit))
+
+# You are the 116877th person to have solved this problem.
--- Python/pythonProject/problem30_tests.py
+++ Python/pythonProject/problem30_tests.py
@@ -1,6 +1,8 @@
 import unittest
 import problem30
 
+limit = 1000000
+
 
 class Problem30Tests(unittest.TestCase):
 
@@ -11,6 +13,7 @@ class Problem30Tests(unittest.TestCase):
 
     def test_is_sum_of_digits_to_power(self):
         for n, expected in [
+            (1, False),
             (5, False),
             (1634, True),
             (1635, False)
@@ -19,9 +22,21 @@ class Problem30Tests(unittest.TestCase):
                 actual = problem30.is_sum_of_digits_to_power(n, 4)
                 self.assertEqual(actual, expected)
 
+    def test_get_numbers_that_are_sum_of_powers_of_their_digits(self):
+        for power, expected in [
+            (1, []),
+            (2, []),
+            (3, [153, 370, 371, 407]),
+            (4, [1634, 8208, 9474]),
+            (5, [4150, 4151, 54748, 92727, 93084, 194979])
+        ]:
+            with self.subTest(p1=power, p2=expected):
+                actual = problem30.get_numbers_that_are_sum_of_powers_of_their_digits(power, limit)
+                self.assertEqual(actual, expected)
+
     def test_solve(self):
-        actual = problem30.solve()
-        expected = 1
+        actual = problem30.solve(limit)
+        expected = 443839
 
         self.assertEqual(actual, expected)
 
